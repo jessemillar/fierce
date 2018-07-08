8036b7b866d9681169f1b81afc323078d07c7b99
Jesse Millar <hellojessemillar@gmail.com>
2018-07-08 11:23:48 -0500

    Better font generation script

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 ## Fierce
 My wife loves [Pusheen](http://pusheen.com/) and I do too. This repo is a [Fitbit Versa](https://www.fitbit.com/shop/versa) watch face featuring Pusheen.
 
+I used the [APNGb](https://github.com/shgodoroja/APNGb) application to take iOS stickers and rip individual frames from the animations.
+
 I do not claim any copyright to Pusheen (which is why I'll never put this watch face on the Fitbit watch face catalog).
--- dev/font.sh
+++ dev/font.sh
@@ -5,23 +5,19 @@ FILL=$2
 SIZE=$3
 SUFFIX=$4
 
-convert -font $FONT -fill $FILL -pointsize $SIZE -transparent white label:: colon$SUFFIX.png
+CHARACTERS=(0 1 2 3 4 5 6 7 8 9 : - Mon Tue Wed Thu Fri Sat Sun)
 
-convert -font $FONT -fill $FILL -pointsize $SIZE -transparent white label:0 0$SUFFIX.png
-convert -font $FONT -fill $FILL -pointsize $SIZE -transparent white label:1 1$SUFFIX.png
-convert -font $FONT -fill $FILL -pointsize $SIZE -transparent white label:2 2$SUFFIX.png
-convert -font $FONT -fill $FILL -pointsize $SIZE -transparent white label:3 3$SUFFIX.png
-convert -font $FONT -fill $FILL -pointsize $SIZE -transparent white label:4 4$SUFFIX.png
-convert -font $FONT -fill $FILL -pointsize $SIZE -transparent white label:5 5$SUFFIX.png
-convert -font $FONT -fill $FILL -pointsize $SIZE -transparent white label:6 6$SUFFIX.png
-convert -font $FONT -fill $FILL -pointsize $SIZE -transparent white label:7 7$SUFFIX.png
-convert -font $FONT -fill $FILL -pointsize $SIZE -transparent white label:8 8$SUFFIX.png
-convert -font $FONT -fill $FILL -pointsize $SIZE -transparent white label:9 9$SUFFIX.png
+for i in ${CHARACTERS[@]}; do
+	CHAR=$i
+	FILENAME=$i
 
-convert -font $FONT -fill $FILL -pointsize $SIZE -transparent white label:Mon Mon$SUFFIX.png
-convert -font $FONT -fill $FILL -pointsize $SIZE -transparent white label:Tue Tue$SUFFIX.png
-convert -font $FONT -fill $FILL -pointsize $SIZE -transparent white label:Wed Wed$SUFFIX.png
-convert -font $FONT -fill $FILL -pointsize $SIZE -transparent white label:Thu Thu$SUFFIX.png
-convert -font $FONT -fill $FILL -pointsize $SIZE -transparent white label:Fri Fri$SUFFIX.png
-convert -font $FONT -fill $FILL -pointsize $SIZE -transparent white label:Sat Sat$SUFFIX.png
-convert -font $FONT -fill $FILL -pointsize $SIZE -transparent white label:Sun Sun$SUFFIX.png
+	if [[ $FILENAME == ":" ]]
+	then
+		FILENAME="colon"
+	elif [[ $FILENAME == "-" ]]
+	then
+		FILENAME="dash"
+	fi
+
+	convert -font $FONT -fill $FILL -pointsize $SIZE -transparent white label:$CHAR $FILENAME$SUFFIX.png
+done
