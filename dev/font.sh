eafd62945beae5b79c9a04dad9b32a10b329f572
Jesse Millar <hellojessemillar@gmail.com>
2018-07-10 08:45:48 -0500

    Much better font script

--- dev/font.sh
+++ dev/font.sh
@@ -5,7 +5,11 @@ FILL=$2
 SIZE=$3
 SUFFIX=$4
 
-CHARACTERS=(0 1 2 3 4 5 6 7 8 9 : - Mon Tue Wed Thu Fri Sat Sun)
+METAFILE="font-data.json"
+
+CHARACTERS=(0 1 2 3 4 5 6 7 8 9 : - pm am Mon Tue Wed Thu Fri Sat Sun)
+
+echo '{"characters": {' > $METAFILE
 
 for i in ${CHARACTERS[@]}; do
 	CHAR=$i
@@ -19,5 +23,17 @@ for i in ${CHARACTERS[@]}; do
 		FILENAME="dash"
 	fi
 
-	convert -font $FONT -fill $FILL -pointsize $SIZE -transparent white label:$CHAR $FILENAME$SUFFIX.png
+	FILENAME=$FILENAME$SUFFIX.png
+
+	convert -font $FONT -fill $FILL -pointsize $SIZE -transparent white label:$CHAR $FILENAME
+
+	echo -n '"'$CHAR'": {"width": ' >> $METAFILE
+	convert $FILENAME -ping -format "%w" info: | xargs echo -n >> $METAFILE
+	echo -n ', "height": ' >> $METAFILE
+	convert $FILENAME -ping -format "%h" info: | xargs echo -n >> $METAFILE
+	echo "}," >> $METAFILE
 done
+
+sed '$ s/.$//' $METAFILE | tee $METAFILE
+
+echo "}}" >> $METAFILE
